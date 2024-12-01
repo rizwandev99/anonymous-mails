af8ae49dc9093fed5813b492d81b5af2212f4769
rizwan <rizwandev99@gmail.com>
2024-12-01 13:06:05 +0530

    completed nextAuth with middleware

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Hello World!!!</div>;
+}
+
+export default page;
--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -49,6 +49,27 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
+  callbacks:{
+    async jwt({token,user}){
+        if(user){
+            token._id = user._id?.toString();
+            // ObjectId to string
+            token.isVerified = user.isVerified;
+            token.isAcceptingMessages = user.isAcceptingMessages;
+            token.username = user.username;
+        }
+        return token
+    },
+    async session ({session,token}){
+        if(token){
+            session.user._id = token._id;
+            session.user.isVerified = token.isVerified;
+            session.user.isAcceptingMessages = token.isAcceptingMessages;
+            session.user.username = token.username;
+        }
+        return session;
+    }
+  },
   session: {
     strategy: "jwt",
   },
