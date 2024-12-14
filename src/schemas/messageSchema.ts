3881f8cdbdb0bc0326bb75cb9a293addb978a412
Md Rizwan <rizwandev99@gmail.com>
2024-12-14 15:22:23 +0530

    V2 initiated

--- src/schemas/messageSchema.ts
+++
@@ -1,6 +0,0 @@
-import {z} from 'zod'
-
-export const messageSchema = z.object({
-    content: z.string().min(10,{message:"content must be 10 characters long"})
-    .max(300,{message:"Content must not be longer than 300 characters "})
-})
