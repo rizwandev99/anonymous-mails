3b8a6625769f2b5349b7b640e75710e98d36e8f4
Md Rizwan <rizwandev99@gmail.com>
2024-12-26 20:43:19 +0530

    User schema created

--- src/models/User.ts
+++ src/models/User.ts
@@ -10,3 +10,36 @@ export interface UserInterface {
   isAcceptingMessages: boolean;
   messages: MessageInterface[];
 }
+
+const UserSchema = new mongoose.Schema<UserInterface>({
+  email: { 
+    type: String, 
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please use a valid email address']
+  },
+  password: { 
+    type: String, 
+    required: [true, 'Password is required'] 
+  },
+  verifyCode: { 
+    type: String, 
+    required: [true, 'Verify code is required'] 
+  },
+  verifyCodeExpiry: { 
+    type: Date, 
+    required: [true, 'Verify code expiry is required'] 
+  },
+  isVerified: { 
+    type: Boolean, 
+    default: false 
+  },
+  isAcceptingMessages: { 
+    type: Boolean, 
+    default: true 
+  },
+  messages: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Message' 
+  }]
+});
