5a33cf80bcfbc531c3c8af8628a21dcd533a1b25
Md Rizwan <rizwandev99@gmail.com>
2025-01-07 21:07:56 +0530

    dbConnect code changes done

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,1 +1,10 @@
 import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/anonymous-mails';
+
+async function dbConnect() {
+  if (mongoose.connection.readyState === 1) return mongoose.connection;
+  return await mongoose.connect(MONGODB_URI);
+}
+
+export default dbConnect;
